a3a8bdecb7474a4cf25434f581b4f5a560af9b06
Mohit Hooda <mhooda87@gmail.com>
2022-12-01 13:33:34 +0530

    FIle modified by removing argsv

--- cli.py
+++ cli.py
@@ -1,29 +1,25 @@
 import calc
 import sys
 
-totalArgs = len(sys.argv)
-if totalArgs < 4 :
-    print('Insufficient argumenets')
-    exit
-else:
-    numA = int(sys.argv[2])
-    numB = int(sys.argv[3])
-    operationChoice = int(sys.argv[1])
 
-    if operationChoice == 1:
-        print(calc.add(numA, numB))
-    
-    elif operationChoice == 2:
-        print(calc.multiply(numA, numB))
-    
-    elif operationChoice == 3:
-        print(calc.subtract(numA, numB))
-    
-    elif operationChoice == 4:
-        if numB == 0:
-            print('Division by 0 - Not allowed')
-        else:
-            print(calc.divide(numA, numB))
-    
+numA = int(input('Enter first number: '))
+numB = int(input('Enter second number: '))
+operationChoice = int(input('Enter operation choice - 1: Addition, 2: Multiply, 3: Subtraction, 4: Division: '))
+
+if operationChoice == 1:
+    print(calc.add(numA, numB))
+
+elif operationChoice == 2:
+    print(calc.multiply(numA, numB))
+
+elif operationChoice == 3:
+    print(calc.subtract(numA, numB))
+
+elif operationChoice == 4:
+    if numB == 0:
+        print('Division by 0 - Not allowed')
     else:
-        print('Inappropriate Operation Choice')
+        print(calc.divide(numA, numB))
+
+else:
+    print('Inappropriate Operation Choice')
